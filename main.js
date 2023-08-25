f7a9751188204c411ff6e96fbe598e7cca3f9209
Barnaby Hall <barnaby-hall@outlook.com>
2023-08-25 21:42:19 +0100

    created first draft (base) of todo list app

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const addButton = document.getElementById("addRowButton");
+    const gridContainer = document.getElementById("todoListContainer");
+
+    addButton.addEventListener("click", function() {
+        //create new row
+        console.log(`Create new row button pressed`)
+    })
+})
